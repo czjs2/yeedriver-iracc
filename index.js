77bcb42ffa4bc9e1f4f53125b288f067e51616ea
fx <czjs2@qq.com>
2017-09-20 15:23:31 +0800

    基本测试成功

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const MBase = ModbusBase.ModbusBase;
  * Created by zhuqizhong on 16-12-2.
  */
 
-const JobQueue = require('qz-jobqueue');
+const JobQueue = require('qz-jobqueue').JobQueue;
 const ModbusRTU = require("qz-modbus-serial");
 const util = require('util');
 
@@ -19,36 +19,114 @@ const IRACC = require('./iracc');
  * sids的说明
  *
  * 第一版本  {devId,devType}
+ *
  */
-function IRACCMaster(maxSegLength, minGapLength) {
-    ModbusBase.call(this, 8, 1);
+class IRACCMaster extends ModbusBase {
+    constructor(maxSegLength, minGapLength){
+        super(8, 1);
 
-    this.jobQueue = new JobQueue({consumer:this.doSendData.bind(this)});
-    this.curCallIndex = 0;
-    this.call_buffer = [];
-    let self = this;
-    function callACState(){
-        self.autoCallHandler = null;
-        if(self.curCallIndex < self.call_buffer.length){
-            let sendData =  _.clone(self.call_buffer[self.curCallIndex]);
-            self.sendCtrl(sendData).then((data)=>{
-                self.devices[sendData.ac_devId] && self.devices[sendData.ac_devId].updateACState(data);
-            }).fin(()=>{
-                this.curCallIndex++;
-                if(this.curCallIndex >= self.call_buffer.length){
-                    self.curCallIndex = 0;
-                }
-                self.autoCallHandler = setTimeout(callACState,200);
-            })
+        this.jobQueue = new JobQueue({consumer:this.doSendData.bind(this)});
+        this.curCallIndex = 0;
+        this.call_buffer = [];
+        let self = this;
+        function callACState(){
+            self.autoCallHandler = null;
+            if(self.curCallIndex < self.call_buffer.length){
+                let sendData =  _.clone(self.call_buffer[self.curCallIndex]);
+                self.sendCtrl(sendData).then((data)=>{
+                    self.devices[sendData.devId] && self.devices[sendData.devId].updateACState(data);
+                    self.emit('RegRead',{devId:devId ,memories:this.autoReadMaps[devId]});
+                }).finally(()=>{
+                    self.curCallIndex++;
+                    if(self.curCallIndex >= self.call_buffer.length){
+                        self.curCallIndex = 0;
+                    }
+                    self.autoCallHandler = setTimeout(function(){callACState();},200);
+                })
+            }
         }
+        this.autoCallHandler = setTimeout(callACState,100);
+
     }
 
-    this.autoCallHandler = setTimeout(callACState,200);
+    initDriver(options) {
+        super.initDriver(options)
+
+        this.sids = options.sids || {}  ;
+
+        this.ids = options.ids.split(',') || [];
+
+        this.call_buffer = [];
+
+        this.drivers = this.drivers || [];
+
+        _.each(options.sids,(data,devId)=>{
+            if (!this.devices[devId]){
+                let matchId = devId.match(/^AC(\d+)_(\d+)/i);
+                if(_.indexOf(this.ids,matchId[1])>=0){
+                    this.devices[devId] = new IRACC(devId, this);
+                    this.call_buffer.push({
+                        devId: devId,
+                        func: 0x04,
+                        ac_devId: matchId[1],
+                        reg_start: 0x07d0 + 6 * matchId[2],
+                        reg_len: 6
+                    });
+                }
+            }
+        })
+
+        setTimeout(()=>{
+            _.each(this.ids,(mbId)=>{
+                let ctrlData = {func: 0x04, ac_devId: mbId, reg_start: 0x01, reg_len: 4};
+                this.sendCtrl(ctrlData).then((data) => {
+                    for (let i = 0; i < 4 && i < (data ? data.length : 0); i++) {
+                        for (let j = 0; j < 16; j++) {
+                            if (data[i] & (1 << j)) {
+                                let index = ((i * 16) + j);
+                                let devId = 'AC' + mbId + "_" + index;
+                                if (this.devices[devId]) {
+                                    this.devices[devId].setConnected(true);
+                                }
+                            }
+
+                        }
+                    }
+                })
+            });
+
+            this.setRunningState(this.RUNNING_STATE.CONNECTED);
+        },500)
+
+        this.setupEvent();
+
+    }
+
+
+    ReadWQ(mapItem,devId){
+        return this.CreateWQReader(mapItem,function(reg){
+            if(!this.devices[devId]){
+                return undefined;
+            }else{
+                return this.devices[devId].ReadWQ(reg);
+            }
+        });
+    }
+
+    WriteWQ(mapItem,values,devId){
+        return this.CreateWQWriter(mapItem,values,function(reg,value){
+            if(this.devices[devId]){
+                return this.devices[devId].WriteWQ(reg,value)
+            }
+        });
+    }
+
+    release(){
+        clearInterval(this.autoCallHandler);
+    };
 }
-util.inherits(IRACCMaster, ModbusBase);
-IRACCMaster.prototype.release = function(){
-    clearInterval(this.autoCallHandler);
-};
+
+
 /**
  *
  * @param options
@@ -58,83 +136,52 @@ IRACCMaster.prototype.release = function(){
  *
  * mb_devId是iracc的控制器地址, acId是里面每一台空调的地址
  */
-IRACCMaster.prototype.initDriver = function (options) {
-    ModbusBase.prototype.initDriver(options);
 
-    this.call_buffer = [];
-    _.each(options.sids, function (data_array, mbId) {
-        for(let i = 0; i < (data_array && data_array.length ); i++){
-            for(let j=0;j<8;j++){
-                let index =((i*8)+j);
-                let devId = 'AC'+mbId+"_"+index;
-                if(!this.devices[devId])
-                    this.devices[devId] = new IRACC(devId, this.mbClient,this);
-                this.call_buffer.push({ac_devId:devId,func:0x04,devId:mbId,reg_start:0x07d0+6*index,reg_len:6});
-            }
-        }
-        //发一个搜索命令，检查设备是否在线
-        let ctrlData = {func:0x04,devId:mbId,reg_start:0x01,reg_len:4};
-        this.sendCtrl(ctrlData).then((data)=>{
-            for(let i = 0; i < 8 && i <(data?data.length:0);i++){
-                for(let j=0;j < 8;j++){
-                    let index =((i*8)+j);
-                    let devId = 'AC'+mbId+"_"+index;
-                    if(this.devices[devId]){
-                        this.devices[devId].setConnected(true);
-                    }
-                }
-            }
-        })
-
-
-    }.bind(this));
-
-
-};
 IRACCMaster.prototype.sendCtrl=function(data){
 
     return this.jobQueue.push(data);
 }
 IRACCMaster.prototype.doSendData = function(data){
     if(this.mbClient){
+        this.mbClient.setID(data.ac_devId);
         switch(data.func){
             case 0x01:
-                return this.mbClient.readCoils(data.ac_devId,data.reg_start, data.reg_len).then(function(newData){
+                return this.mbClient.readCoils(data.reg_start, data.reg_len).then(function(newData){
                     return newData.data;
                 });
                 break;
             case 0x02:
-                return this.mbClient.readDiscreteInputs(data.ac_devId,data.reg_start, data.reg_len).then(function(newData){
+                return this.mbClient.readDiscreteInputs(data.reg_start, data.reg_len).then(function(newData){
                     return newData.data;
                 });
                 break;
             case 0x03:
-                return this.mbClient.readHoldingRegisters(data.ac_devId,data.reg_start, data.reg_len).then(function(newData){
+                return this.mbClient.readHoldingRegisters(data.reg_start, data.reg_len).then(function(newData){
                     return newData.data;
                 });
                 break;
             case 0x04:
-                return this.mbClient.readInputRegisters(data.ac_devId,data.reg_start, data.reg_len).then(function(newData){
+                return this.mbClient.readInputRegisters(data.reg_start, data.reg_len).then(function(newData){
                     return newData.data;
                 });
                 break;
             case 0x05:
-                return this.mbClient.writeCoil(data.ac_devId,data.reg_addr, data.reg_value).then(function(newData){
+                return this.mbClient.writeCoil(data.reg_addr, data.reg_value).then(function(newData){
                     return newData.data;
                 });
                 break;
             case 0x06:
-                return this.mbClient.writeRegister(data.ac_devId,data.reg_addr, data.reg_value).then(function(newData){
+                return this.mbClient.writeRegister(data.reg_addr, data.reg_value).then(function(newData){
                     return newData.data;
                 });
                 break;
             case 0x0f:
-                return this.mbClient.writeCoils(data.ac_devId,data.reg_start, data.reg_values).then(function(newData){
+                return this.mbClient.writeCoils(data.reg_start, data.reg_values).then(function(newData){
                     return newData.data;
                 });
                 break;
             case 0x10:
-                return this.mbClient.writeRegisters(data.ac_devId,data.reg_start, data.reg_values).then(function(newData){
+                return this.mbClient.writeRegisters(data.reg_start, data.reg_values).then(function(newData){
                     return newData.data;
                 });
                 break;
@@ -149,24 +196,32 @@ IRACCMaster.prototype.doSendData = function(data){
 IRACCMaster.prototype.setInOrEx = function () {
     let addDevices = {};
     let delDevices = {};
-    _.each(this.options.sids, function (data_array, mbId) {
+    _.each(this.ids, function (mbId) {
         //发一个搜索命令，检查设备是否在线
-        let ctrlData = {func:0x04,devId:mbId,reg_start:0x01,reg_len:4};
+        let ctrlData = {func:0x04,ac_devId:mbId,reg_start:0x01,reg_len:4};
 
         this.sendCtrl(ctrlData).then((data)=>{
-            for(let i = 0; i < 8 && i <(data?data.length:0);i++){
-                for(let j=0;j < 8;j++){
-                    let index =((i*8)+j);
+            for(let i = 0; i < 4 && i <(data?data.length:0);i++){
+                for(let j=0;j < 16;j++){
+                    let index =((i*16)+j);
                     let devId = 'AC'+mbId+"_"+index;
-                    if( (data[i] & (0x01<<j)) !== (data_array[i] & (0x01<<j)) ){
-                        if(data[i] & (0x01<<j)){ //这一个设备是新的
-                            addDevices[devId] =  {
-                                uniqueId:'iracc',
-                                groupId:".",
-                            }
-                        }else{
-                            delDevices[devId] = 'iracc'
+                    if(!this.sids[devId] && (data[i] & (0x01<<j)) ){
+                        addDevices[devId] =  {
+                            uniqueId:'ac',
+                            groupId:".",
                         }
+                        this.devices[devId] = new IRACC(devId, this);
+                        this.call_buffer.push({
+                            devId: devId,
+                            func: 0x04,
+                            ac_devId: mbId,
+                            reg_start: 0x07d0 + 6 * index,
+                            reg_len: 6
+                        });
+
+                    }
+                    if(this.sids[devId] && !(data[i] & (0x01<<j)) ){
+                        delDevices[devId] = 'ac'
                     }
                 }
             }
--- iracc.js
+++ iracc.js
@@ -2,8 +2,7 @@
  * Created by zhuqizhong on 17-5-15.
  */
 
-const ModbusBase = require('yeedriver-modbustcpconv').ModbusBase;
-const async = require('async-q');
+const ModbusBase = require('yeedriver-modbustcpconv');
 const _ = require('lodash');
 const MODE={
     UNDEF:0,
@@ -13,6 +12,15 @@ const MODE={
     HUMITY:4, //除湿
     AUTO:5
 };
+
+const MODE_VALUE={
+    1:0,  //风机
+    2:1,  //制热
+    3:2,  //制冷
+    4:7, //除湿
+    5:3
+};
+
 const SENSE_STATE={
     UNKNOWN:0,
     NORMAL:1,
@@ -20,23 +28,23 @@ const SENSE_STATE={
 };
 
 const WQ_ID={
-    power:0,
-    ventile:1,
+    power:1,
     mode:2,
     target_temp:3,
-    house_temp:4,
+    ventile:4,
     filter_req:5,
-    sensor_state:6,
-    fault_code:7,
+    fault_code:6,
+    sensor_state:7,
+    house_temp:8,
     connected:60001
 
 };
 
 const FAULT_C1 = ['0','1','2','3','4','5','6','7','8','9','A','H','C','J','E','F'];
 const FAULT_C2 = ['0','A','C','E','H','F','J','L','P','U','9','8','7','6','5','4','3','3','2','1','G','K','M','N','R','T','V','W','X','Y','Z','*',' ']
-class  IRACC extends ModbusBase{
-    constructor(devId,mbClient,writer) {
-        super(devId,mbClient);
+class  IRACC  {
+    constructor(devId,writer) {
+
 
         this.devId = devId;
         this.ventile = undefined;  //风量 从1到5  归一化成20-100
@@ -59,11 +67,11 @@ class  IRACC extends ModbusBase{
      * @param data
      */
     updateACState(data){
-        if(data && data.length >= 12){
-            this.ventile = (data[0] >> 4)*20;
-            this.power = !!data[1];
-            this.filterReq =  (data[2]=== 0x42);
-            switch(data[3]&0x0f){
+        if(data && data.length >= 6){
+            this.ventile = data[0] >> 12; //风量
+            this.power = !!(data[0]&0xff);
+            this.filter_req =  ((data[1]>>8)=== 0x42); //过虑网清洗标志
+            switch(data[1]&0x0f){
                 case 0:
                     this.mode = MODE.VENT;
                     break;
@@ -73,41 +81,30 @@ class  IRACC extends ModbusBase{
                 case 2:
                     this.mode = MODE.COLD;
                      break;
+                case 3:
+                    this.mode = MODE.AUTO;
+                    break;
                 case 0x07:
                     this.mode = MODE.HUMITY;
                     break;
             }
-            this.target_temp = (data[4]*100+data[5])/10;
-            this.fault_code = FAULT_C2[(data[6]&0x1f)] + FAULT_C1[data[7]&0x0F];
-            this.house_temp = (data[8]*100+data[9])/10;
-            this.sensor_state = ((data[10])===0x80);
+            this.target_temp = data[2]/10;
+            this.fault_code = FAULT_C2[(data[3]>>8)] + FAULT_C1[data[3]&0xff];
+            this.house_temp = data[4]/10;
+            this.sensor_state = ((data[5])==0x0000);
             this.connected = true;
         }
 
 
     }
 
-    WriteWQ(mapItem, value) {
-
-        let reg_quantity = (mapItem.end - mapItem.start + 1);
-
-        let regs = [];
-        for (let i = 0; i < reg_quantity; i++) {
-            let wq_id =mapItem.start + i;
-            regs.push(wq_id);
-
-
-        }
-        return async(regs,function(reg){
-            return this.CtrlAC(reg,value[reg]);
-        }.bind(this))
-
-
+    WriteWQ(reg, value) {
+        return this.CtrlAC(reg,value);
     }
 
-    ReadWQ (mapItem){
+    ReadWQ (reg){
 
-        return this.CreateWQReader(mapItem,function(reg,results){
+
             switch(reg){
                 case WQ_ID.power: //PH
                     return this.power;
@@ -136,33 +133,37 @@ class  IRACC extends ModbusBase{
                 case WQ_ID.connected:
                     return this.connected;
                     break;
+                default:
+                    return undefined;
+                    break;
             }
-        });
+
 
     };
 
     setConnected(state){
         this.connected = state;
     }
-   CtrlAC(wq_id,value){
+    CtrlAC(wq_id,value){
 
     if(this.address && this.address.length >= 3){
         let writeBuf = {
             func:0x06,
-            devId:parseInt(this.address[1]),
-            reg_start:parseInt(this.address[2]*3+0x7d0),
-            reg_len:1
+            ac_devId:parseInt(this.address[1]),
+
         };
         switch(wq_id){
             case WQ_ID.power: //PH
-                writeBuf.data=[0xff,!!value?0x61:0x60];
+                writeBuf.reg_addr=parseInt(this.address[2]*3+0x07d0);
+                writeBuf.reg_value=(0xff<<8)+((value?0x61:0x60));
                 break;
             case WQ_ID.ventile:
-                writeBuf.data=[parseInt(value/20)<<4,0xff];
+                writeBuf.reg_addr=parseInt(this.address[2]*3+0x07d0);
+                writeBuf.reg_value=(parseInt(value)<<12)+0xff;
                 break;
             case WQ_ID.mode:
                 let mode = 0;
-                writeBuf.reg_start = parseInt(this.address[2]*3+0x7d0) +1;
+                writeBuf.reg_addr = parseInt(this.address[2]*3+0x7d0) +1;
                 switch(value){
                     case  MODE.VENT:
                         mode = 0;
@@ -180,20 +181,46 @@ class  IRACC extends ModbusBase{
                         mode = 3;
                         break;
                 }
-                writeBuf.data=[0x00,mode];
+                writeBuf.reg_value=mode;
                 break;
             case WQ_ID.target_temp:
-                writeBuf.reg_start = parseInt(this.address[2]*3+0x7d0) +1;
+                writeBuf.reg_addr = parseInt(this.address[2]*3+0x7d0) +2;
                 let temp_val =parseInt(value*10);
-                writeBuf.data=[(temp_val>>8)&0xff,temp_val&0xFF];
+                writeBuf.reg_value=temp_val;//(temp_val>>8)&0xff,temp_val&0xFF];
                 break;
         }
+
+        // let writeBuf = {
+        //     func:0x10,
+        //     ac_devId:parseInt(this.address[1]),
+        //     reg_start:parseInt(this.address[2]*3+0x07d0),
+        //     reg_values:[]
+        //
+        // };
+        // writeBuf.reg_values.push((this.ventile<<12) + (this.power?0x61:0x60));
+        // writeBuf.reg_values.push(MODE_VALUE[this.mode]||0);
+        // writeBuf.reg_values.push(this.target_temp*10);
+        //
+        // switch(wq_id){
+        //     case WQ_ID.power: //PH
+        //         writeBuf.reg_values[0]=(writeBuf.reg_values[0] & 0xff00) + (value?0x61:0x60);
+        //         break;
+        //     case WQ_ID.ventile:
+        //         writeBuf.reg_values[0]=(parseInt(value)<<12)+(writeBuf.reg_values[0]&0x0fff);
+        //         break;
+        //     case WQ_ID.mode:
+        //         writeBuf.reg_values[1]=MODE_VALUE[value || 0];
+        //         break;
+        //     case WQ_ID.target_temp:
+        //         writeBuf.reg_values[2]=value*10;
+        //         break;
+        // }
 
         return this.mbWriter.sendCtrl(writeBuf);
     }else{
         return Q.reject(`error of devId:${this.devId} `);
     }
 
-}
+    }
 }
 module.exports = IRACC;
